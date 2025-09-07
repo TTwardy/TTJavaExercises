2e1d52f8c34c59f7448b881011ed3892dae29ce7
Tomasz <tgtwardy@gmail.com>
2025-09-07 17:32:43 +0200

    Chapter15 - file matching

---
+++ src/main/java/Chapter15/telephoneNumbersToWords/NumberToWordsProcessor.java
@@ -0,0 +1,37 @@
+package Chapter15.telephoneNumbersToWords;
+
+import java.util.Formatter;
+
+public class NumberToWordsProcessor {
+    static String[] keypad = {
+            "",
+            "",
+            "ABC",
+            "DEF",
+            "GHI",
+            "JKL",
+            "MNO",
+            "PRS",
+            "TUV",
+            "WXY"
+    };
+    String phoneNumber;
+
+    public NumberToWordsProcessor(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    protected void generateCombinations(int index, String currentWord, Formatter formatter) {
+        if (index == phoneNumber.length()) {
+            formatter.format("%s%n", currentWord);
+            return;
+        }
+        int digit = Character.getNumericValue((phoneNumber).charAt(index));
+        String letters = keypad[digit];
+
+        for (int i = 0; i < letters.length(); i++) {
+            char letter = letters.charAt(i);
+            generateCombinations(index + 1, currentWord + letter, formatter);
+        }
+    }
+}
---
+++ src/main/java/Chapter15/telephoneNumbersToWords/TelephoneNumbersToWordsMain.java
@@ -0,0 +1,32 @@
+package Chapter15.telephoneNumbersToWords;
+
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class TelephoneNumbersToWordsMain {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        NumberToWordsProcessor numberToWordsProcessor = new NumberToWordsProcessor(requestTelephoneNumber());
+
+        try (Formatter formatter = new Formatter("wordFromNumbers.txt")){
+            numberToWordsProcessor.generateCombinations(0,"", formatter);
+        }
+    }
+
+    protected static String requestTelephoneNumber(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the telephone number");
+        while (true){
+            if (scanner.hasNextInt()){
+                return scanner.nextLine();
+            }
+            else {
+                System.out.println("Enter correct number");
+                scanner.nextLine();
+            }
+        }
+
+        //add validations
+    }
+}
